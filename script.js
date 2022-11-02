d95a5f20b441ca855b6ca334783b9eb607ea824a
Aleksandr B <defast2293@gmail.com>
2022-11-02 20:30:40 +0500

    eddit file js, cycle

--- script.js
+++ script.js
@@ -1,21 +1,51 @@
 'use strict';
 
-const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+//const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
 
-const porsonalMovieDB = {
-	count: numberOfFilms,
+//const personalMovieDB = {
+//	count: numberOfFilms,
+//	movies: {},
+//	actors: {},
+//	genres: [],
+//	private: false,
+//};
+
+//const questionFirst = prompt('Один из последних просмотренных фильмов?', '');
+//const questionSecond = prompt('На сколько оцените его?', '');
+//const questionThree = prompt('Один из последних просмотренных фильмов?', '');
+//const questionFor = prompt('На сколько оцените его?', '');
+
+//personalMovieDB.movies[questionOne] = questionTwo;
+//personalMovieDB.movies[questionThree] = questionFor;
+
+//console.log(porsonalMovieDB);
+
+const personalMovieDB = {
 	movies: {},
 	actors: {},
 	genres: [],
 	private: false,
 };
 
-const questionOne = prompt('Один из последних просмотренных фильмов?', '');
-const questionTwo = prompt('На сколько оцените его?', '');
-const questionThree = prompt('Один из последних просмотренных фильмов?', '');
-const questionFor = prompt('На сколько оцените его?', '');
+const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+personalMovieDB.count = numberOfFilms;
+if (personalMovieDB.count < 10) {
+	alert('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+	alert('Вы классический зритель');
+} else if (personalMovieDB.count > 30) {
+	alert('Вы киноман');
+} else {
+	alert('Произошла ошибка');
+}
+for (let i = 0; i < 2; i++) {
+	const questionFirst = prompt('Один из последних просмотренных фильмов?', '');
+	const questionSecond = prompt('На сколько оцените его?', '');
+	if (questionFirst != '' && questionFirst != null && questionFirst.length < 50 && questionSecond != '' && questionSecond != null && questionSecond.length < 50) {
+		personalMovieDB.movies[questionFirst] = questionSecond;
+	} else {
+		i--;
+	}
+}
 
-porsonalMovieDB.movies[questionOne] = questionTwo;
-porsonalMovieDB.movies[questionThree] = questionFor;
-
-console.log(porsonalMovieDB);
+console.log(personalMovieDB);
